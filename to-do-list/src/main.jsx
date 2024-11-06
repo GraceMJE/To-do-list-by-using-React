75e5f60639db407ed8af11368afa2fd0efebcce4
GraceMJE <121802893+GraceMJE@users.noreply.github.com>
2024-11-06 08:53:33 +0000

    contextProvider로 최상위 루트(App) 감싸줘서 contextAPI 사용가능토록 만들어주기

--- to-do-list/src/main.jsx
+++ to-do-list/src/main.jsx
@@ -2,5 +2,10 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { TodoContextProvider } from './context/TodoContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <TodoContextProvider>
+    <App />
+  </TodoContextProvider>
+);
